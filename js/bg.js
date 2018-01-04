219c5ea055d7acd4eb02390b5becebdfff2c8a72
Maik Macho <nuda1998@gmail.com>
2018-01-04 21:48:17 +0100

    ​

--- js/bg.js
+++ js/bg.js
@@ -61,147 +61,179 @@ ${gl.getShaderInfoLog(shader)}
 var shaderProgram;
 var programInfo;
 var positionBuffer;
+var noiseWidth;
+var noiseHeight;
+var noiseTexture;
 
 var renderId;
 
+function generateNoise(width, height) {
+    width--;
+    width |= width >> 1;
+    width |= width >> 2;
+    width |= width >> 4;
+    width |= width >> 8;
+    width |= width >> 16;
+    width++;
+
+    height--;
+    height |= height >> 1;
+    height |= height >> 2;
+    height |= height >> 4;
+    height |= height >> 8;
+    height |= height >> 16;
+    height++;
+
+    if (noiseTexture && width <= noiseWidth && height <= noiseHeight)
+        return;
+
+    if (!noiseTexture)
+        noiseTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, noiseTexture);
+    noiseWidth = width;
+    noiseHeight = height;
+
+    data = [];
+    for (var i = 0; i < 4 * width * height; i++) {
+        data[i] = Math.floor(256 * Math.random());
+    }
+
+    gl.texImage2D(
+        gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+        new Uint8Array(data)
+    );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
 function buildContext() {
+    canvas.width = 0;
+    canvas.height = 0;
+
     var vertexShader = loadShader(gl, gl.VERTEX_SHADER,
-        `#version 100
-        precision highp float;
-        
-        attribute vec4 aVertexPosition;
-        
-        uniform mat4 uModelViewMatrix;
-        uniform mat4 uProjectionMatrix;
-        
-        uniform vec4 uBody;
-        uniform float uEdgeScale;
-        uniform float uEdgeWidth;
-        uniform float uEdgeCount;
-        uniform float uTime;
-        uniform vec2 uMouse;
-        
-        varying vec4 vBody;
-        varying float vEdgeScale;
-        varying float vEdgeWidth;
-        varying float vEdgeCount;
-        varying float vTime;
-        varying vec2 vUV;
-        varying vec2 vMouse;
-        
-        void main() {
-            vBody = uBody;
-            vEdgeScale = uEdgeScale;
-            vEdgeWidth = uEdgeWidth;
-            vEdgeCount = uEdgeCount;
-            vTime = uTime;
-            vUV = aVertexPosition.xy;
-            vMouse = uMouse;
-            
-            gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        }
-        `);
+`#version 100
+precision highp float;
+
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+uniform vec4 uBody;
+uniform vec3 uEdge;
+uniform float uTime;
+uniform vec2 uMouse;
+
+varying vec4 vBody;
+varying vec3 vEdge;
+varying float vTime;
+varying vec2 vUV;
+varying vec2 vMouse;
+
+void main() {
+    vBody = uBody;
+    vEdge = uEdge;
+    vTime = uTime;
+    vUV = aVertexPosition.xy;
+    vMouse = uMouse;
+    
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+`);
     var fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER,
-        `#version 100
-        precision highp float;
-        
-        #define PI 3.14159
-        #define TAU 6.28318
-        
-        #define round(x) (floor(x))
-        
-        varying vec4 vBody;
-        varying float vEdgeScale;
-        varying float vEdgeWidth;
-        varying float vEdgeCount;
-        varying float vTime;
-        varying vec2 vUV;
-        varying vec2 vMouse;
+`#version 100
+precision highp float;
 
-        float random(vec2 p) {
-            return fract(cos(dot(p, vec2(23.14069263277926, 2.665144142690225))) * 123456.0);
-        }
-        
-        float get(vec3 v) {
-            float a = 0.5 + 0.5 * cos(sin(v.x * 3.8) + sin(v.y * 3.8));
-            float b = 0.5 + 0.5 * sin(v.x * 0.17 + v.z * 4.0) * sin(v.y * 0.17);
-            float c = 0.5 + 0.5 * sin(v.x * 2.0 - 1.0 * sin(v.y * 2.0) + v.z * 0.7);
-            float d = 0.5 + 0.5 * sin(v.x + a) * sin(v.y + b) * cos(b + c + 0.1 * v.z);
-            
-            return a * c + b * d;
-        }
-        
-        float crush(float f, float c) {
-            return round(c * f);
-        }
-        
-        void main() {
-            vec2 uv = vUV;
-            float t = vTime;
-        
-            // vec2 m = 0.5 + 0.5 * sin(0.5 * PI * (vMouse - 0.5));
-            vec2 m = vMouse;
+#define PI 3.14159
+#define TAU 6.28318
 
-            float grow = 1.0 - sin(PI * 0.5 * min(1.0, max(0.0, 8.0 * distance(uv, m))));
+#define round(x) (floor(x+0.5))
+#define crush(x,c) (floor(x*c+0.5))
 
-            uv += 0.2 * m;
-        
-            uv += 256.0;
+uniform sampler2D uSamplerNoise;
 
-            uv.y += t;
-        
-            uv *= vEdgeWidth * 16.0;
-        
-            t += 0.1 * m.x;
-            t += 0.1 * m.y;
-        
-            float cc = get(vec3(uv, t));
-            cc = max(0.1, min(0.9, cc));
-        
-            float edgef = vEdgeScale * vEdgeWidth * 0.01;
-            edgef *= 1.0 + grow * 5.0 * (cc * 2.0);
+varying vec4 vBody;
+varying vec3 vEdge;
+#define vEdgeScale (vEdge.x)
+#define vEdgeWidth (vEdge.y)
+#define vEdgeCount (vEdge.z)
+varying float vTime;
+varying vec2 vUV;
+varying vec2 vMouse;
 
-            float body =
-                smoothstep(-edgef, 0.0, vUV.x - vBody.x) *
-                (1.0 - smoothstep(0.0, edgef, vUV.y - vBody.y)) *
-                (1.0 - smoothstep(0.0, edgef, vUV.x - vBody.z)) *
-                smoothstep(-edgef, 0.0, vUV.y - vBody.w)
-            ;
+float get(vec3 v) {
+    float a = cos(sin(v.x * 3.8) + sin(v.y * 3.8));
+    float b = sin(v.x * 0.17 + v.z * 4.0) * sin(v.y * 0.17);
+    float c = sin(v.x - sin(v.y) + v.z);
+    float d = sin(v.x + a) * sin(v.y + b) * cos(b + c + 0.1 * v.z);
+    
+    return 1.0 + 0.2 * a + 0.5 * a * c + 0.4 * c + 0.5 * b * d;
+}
 
-            float edgec = vEdgeCount * 8.0;
+void main() {
+    vec2 uv = vUV;
+    float t = vTime;
 
-            // float cl = crush(edgec, get(vec3(uv.x - edgef, uv.y, t)));
-            // float cr = crush(edgec, get(vec3(uv.x + edgef, uv.y, t)));
-            // float cu = crush(edgec, get(vec3(uv.x, uv.y - edgef, t)));
-            // float cd = crush(edgec, get(vec3(uv.x, uv.y + edgef, t)));
-        
-            float clu = crush(edgec, get(vec3(uv.x - edgef, uv.y - edgef, t)));
-            float cru = crush(edgec, get(vec3(uv.x + edgef, uv.y - edgef, t)));
-            float cld = crush(edgec, get(vec3(uv.x - edgef, uv.y + edgef, t)));
-            float crd = crush(edgec, get(vec3(uv.x + edgef, uv.y + edgef, t)));
-        
-            float d = smoothstep(0.0, 1.0 + body,
-                // abs(cl - cr) +
-                // abs(cu - cd) +
-                abs(clu - crd) +
-                abs(cld - cru) +
-                0.0 // step(0.001, fract(body))
-            );
+    // vec2 m = 0.5 + 0.5 * sin(0.5 * PI * (vMouse - 0.5));
+    vec2 m = vMouse;
 
-            body = step(0.0001, body);
-        
-            float c = (0.05 + cc * 0.95) * d;
-            c = 0.05 * cc + c;
+    float grow = pow(1.0 - min(1.0, max(0.0, 8.0 * distance(uv, m))), 2.0);
 
-            c = (1.0 - body) * c + body * (
-                0.1 + 0.2 * min(1.0, cc + c)
-            );
+    uv += 0.2 * m;
 
-            c += random(vUV * 3.0) * 0.01;
-        
-            gl_FragColor = vec4(c, c, c, 1.0);
-        }
-        `);
+    uv.y += t;
+
+    uv *= vEdgeWidth * 16.0;
+
+    t += 0.1 * m.x + 0.1 * m.y;
+
+    float cc = get(vec3(uv, t));
+    cc = 0.1 + min(0.8, cc);
+
+    float edgef = vEdgeScale * vEdgeWidth * 0.01;
+    edgef *= 1.0 + grow * 5.0 * (cc * 2.0);
+
+    float growbf = 0.02 * grow;
+    float softbody =
+        smoothstep(-growbf, 0.0, vUV.x - vBody.x) *
+        (1.0 - smoothstep(0.0, growbf, vUV.y - vBody.y)) *
+        (1.0 - smoothstep(0.0, growbf, vUV.x - vBody.z)) *
+        smoothstep(-growbf, 0.0, vUV.y - vBody.w) *
+    1.0;
+    float body = smoothstep(0.0001, 0.04, softbody);
+
+    float edgec = vEdgeCount * 8.0 + 0.5 * vUV.x * fract(1.0 - fract(softbody)) * body;
+
+    // float cl = crush(edgec, get(vec3(uv.x - edgef, uv.y, t)));
+    // float cr = crush(edgec, get(vec3(uv.x + edgef, uv.y, t)));
+    // float cu = crush(edgec, get(vec3(uv.x, uv.y - edgef, t)));
+    // float cd = crush(edgec, get(vec3(uv.x, uv.y + edgef, t)));
+
+    float clu = crush(edgec, get(vec3(uv.x - edgef, uv.y - edgef, t)));
+    float cru = crush(edgec, get(vec3(uv.x + edgef, uv.y - edgef, t)));
+    float cld = crush(edgec, get(vec3(uv.x - edgef, uv.y + edgef, t)));
+    float crd = crush(edgec, get(vec3(uv.x + edgef, uv.y + edgef, t)));
+
+    float d = smoothstep(0.0, 1.0,
+        // abs(cl - cr) +
+        // abs(cu - cd) +
+        abs(clu - crd) +
+        abs(cld - cru)
+    );
+
+    float c = (0.05 + cc * 0.95 + grow) * d;
+    c = 0.05 * cc + c;
+
+    c = (1.0 - body) * c + body * (
+        0.1 + 0.2 * min(1.0, 0.3 * cc + c)
+    );
+
+    c += 0.01 * texture2D(uSamplerNoise, vUV).a;
+
+    gl_FragColor = vec4(c, c, c, 1.0);
+}
+`);
         
     shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
@@ -219,10 +251,9 @@ function buildContext() {
         uniformLocations: {
             projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
             modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+            samplerNoise: gl.getUniformLocation(shaderProgram, 'uSamplerNoise'),
             body: gl.getUniformLocation(shaderProgram, 'uBody'),
-            edgeScale: gl.getUniformLocation(shaderProgram, 'uEdgeScale'),
-            edgeWidth: gl.getUniformLocation(shaderProgram, 'uEdgeWidth'),
-            edgeCount: gl.getUniformLocation(shaderProgram, 'uEdgeCount'),
+            edge: gl.getUniformLocation(shaderProgram, 'uEdge'),
             time: gl.getUniformLocation(shaderProgram, 'uTime'),
             mouse: gl.getUniformLocation(shaderProgram, 'uMouse'),
         },
@@ -247,14 +278,13 @@ function buildContext() {
 }
 canvas.addEventListener('webglcontextlost', e => {
     console.log("die");
-    cancelRequestAnimationFrame(renderId);
+    cancelAnimationFrame(renderId);
     event.preventDefault();
 }, false);
 canvas.addEventListener('webglcontextrestored', e => {
     console.log("revive");
+    noiseTexture = null;
     gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-    canvas.width = 0;
-    canvas.height = 0;
     buildContext();
 }, false);
 buildContext();
@@ -262,27 +292,32 @@ buildContext();
 var nowoffs = Date.now();
 var dropped = 1;
 var perfscale = 1;
+var frameskip = 0;
+var frameskipped = 0;
 var then;
 function render(now) {
+    if (frameskipped < frameskip) {
+        frameskipped++;
+        renderId = requestAnimationFrame(render);
+        return;
+    }
+    var fsf = (1 + frameskipped);
     if (!then)
-        then = now - 1000 / 60;
-    if (now - then > 1000 / 30)
+        then = now - 1000 / 60 * fsf;
+    if (now - then > 1000 / 30 * fsf)
         dropped++;
-    else if (now - then < 1000 / 50)
+    else if (now - then < 1000 / 50 * fsf)
         dropped--;
-    if (dropped > 10) {
-        perfscale += 0.25;
+    if (dropped > 5) {
+        perfscale += 0.75;
         dropped = 0;
-    } else if (dropped < -10) {
-        perfscale = Math.max(0.5, perfscale * 0.75);
+    } else if (dropped < -5) {
+        perfscale = Math.max(1.0, perfscale * 0.9);
         dropped = 0;
     }
     then = now;
+    frameskipped = 0;
     
-    gl.clearColor(0.0, 0.0, 0.0, 0.5);
-    gl.clearDepth(1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     var aspect = canvas.clientWidth / canvas.clientHeight;
     
     gl.useProgram(programInfo.program);
@@ -331,18 +366,16 @@ function render(now) {
             modelViewMatrix
         );
 
-        gl.uniform1f(
-            programInfo.uniformLocations.edgeScale,
-            Math.max(1.0, perfscale)
+        gl.uniform3fv(
+            programInfo.uniformLocations.edge,
+            [
+                Math.max(1.0, perfscale), // Scale
+                100 / Math.min(width, height), // Width
+                Math.min(width, height) / 700 // Count
+            ]
         );
-        gl.uniform1f(
-            programInfo.uniformLocations.edgeWidth,
-            100 / Math.min(width, height)
-        );
-        gl.uniform1f(
-            programInfo.uniformLocations.edgeCount,
-            Math.min(width, height) / 700
-        );
+
+        generateNoise(canvas.width, canvas.height);
     }
 
     
@@ -365,6 +398,10 @@ function render(now) {
         programInfo.uniformLocations.mouse,
         [vwidth * mouse[0], vheight * mouse[1]]
     );
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, noiseTexture);
+    gl.uniform1i(programInfo.uniformLocations.samplerNoise, 0);
     
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
---
+++ js/bgm.js
@@ -0,0 +1,75 @@
+(()=>{
+
+var context = new AudioContext();
+
+var samples = {};
+var samplemap = {};
+var irs = {};
+var irmap = {};
+
+var fetching = 0;
+function fetchSample(url) {
+    fetching++;
+    var target = samples;
+    if (url.startsWith('assets/irs/'))
+        target = irs;
+    fetch(url)
+        .then(response => response.arrayBuffer())
+        .then(data => context.decodeAudioData(data))
+        .then(sample => {
+            for (var i = 0; i < arguments.length; i++)
+                target[arguments[i]] = sample;
+            fetching--;
+            return sample;
+        });
+}
+
+var masterCompressor = context.createDynamicsCompressor();
+masterCompressor.connect(context.destination);
+
+var masterGain = context.createGain();
+masterGain.connect(masterCompressor);
+
+var masterFilter = context.createBiquadFilter();
+masterFilter.frequency.value = context.sampleRate * 0.5;
+masterFilter.connect(masterGain);
+
+var masterConvolverBypass = context.createGain();
+masterConvolverBypass.connect(masterFilter);
+
+var masterConvolver = context.createConvolver();
+masterConvolver.connect(masterFilter);
+
+var masterConvolverGain = context.createGain();
+masterConvolverGain.connect(masterConvolver);
+
+masterConvolverGain.gain.value = 0.5;
+masterConvolverBypass.gain.value = 0.9;
+
+var master = context.createGain();
+master.connect(masterConvolverGain);
+master.connect(masterConvolverBypass);
+
+fetching++;
+fetch('assets/samplemap.json').then(response => response.json())
+    .then(map => {
+        samplemap = map;
+        map.samples.forEach(full => {
+            var short = full.split('/');
+            fetchSample(`assets/samples/${full}.ogg`, full, short[short.length - 1]);
+        });
+        fetching--;
+    });
+
+fetching++;
+fetch('assets/irmap.json').then(response => response.json())
+    .then(map => {
+        irmap = map;
+        map.forEach(full => {
+            var short = full.split('/');
+            fetchSample(`assets/irs/${full}.wav`, full, short[short.length - 1]);
+        });
+        fetching--;
+    });
+
+})();
